3e7f7ea37e735b723c99dcab95d6a7ed86a509a4
Moonjang <138779045+ParalMoon@users.noreply.github.com>
2025-11-13 13:44:09 +0000

    update

--- ex1.ml
+++ ex1.ml
@@ -1,16 +1,11 @@
 (* gcd *)
 
 let rec gcd n m =
-  (* Case: both zero -> return 0 by definition in test cases *)
   if n = 0 && m = 0 then 0
-  (* Ensure n >= m 조건을 만족시키기 위해 swap *)
   else if n < m then gcd m n
-  (* Euclid base case *)
   else if m = 0 then n
-  (* Recursive subtraction *)
   else gcd (n - m) m
 
-(* simple test printing *)
 let () =
   print_endline (string_of_int (gcd 10 0));
   print_endline (string_of_int (gcd 9 5));
